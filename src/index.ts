a6a843f5ef3de663b3d92aea21b5edc6e18df94a
sxhmih <schmitt.h@northeastern.edu>
2023-09-12 15:23:57 -0400

    add subtract operation

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,7 @@ const calc = new Calculator();
 const result_add = calc.add(2, 3);
 console.log(`result_add = ${result_add}`);
 
+const result_subtract = calc.subtract(5,3);
+console.log(`result_subtract = ${result_subtract}`);
+
+
