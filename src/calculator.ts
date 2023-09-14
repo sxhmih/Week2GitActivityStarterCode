cad5fdacb1338f4a71241a0f52ee6768a7a9733b
sxhmih <schmitt.h@northeastern.edu>
2023-09-14 15:12:48 -0400

    print hello message

--- src/calculator.ts
+++ src/calculator.ts
@@ -11,16 +11,15 @@ export class Calculator {
 
 
 }  
- public divide(x:number, y:number) : number {
-        return x/y;
-
-}
+ 
 
 
   public divide(x: number, y:number) :number {
       return x / y;
 }
-
+ public hello() {
+ console.log("hello")
+}
 
 };
 
